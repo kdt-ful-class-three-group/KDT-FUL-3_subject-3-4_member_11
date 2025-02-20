2b8a35ee28dbcb0af3028da77d14bcbe405f8ada
JeonSunil <jeonsunil1225@gmail.com>
2025-02-20 11:40:14 +0900

    보기 편하게 내용을 간단히 모듈화 함

---
+++ addText.js
@@ -0,0 +1,10 @@
+function addText() {
+  return `<a href = "/">홈</a><a href = "/list">글 목록</a><a href = "/add">글 작성</a>
+                  <form action="/data" method="post">
+                    <input type="text" name="name" placeholder="name">
+                    <input type="text" name="main" placeholder="main">
+                    <button type="submit">작성</button>
+                  </form>`
+}
+
+module.exports = addText;
