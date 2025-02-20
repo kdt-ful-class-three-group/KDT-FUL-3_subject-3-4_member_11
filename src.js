da7020d4def664c5794b617491388321d2042656
JeonSunil <jeonsunil1225@gmail.com>
2025-02-20 17:51:06 +0900

    data.json을 불러와서, for문으로 dom제어를 해서, data.json의 배열만큼 li태그를 생성하게 만들었다.

--- src.js
+++ src.js
@@ -3,7 +3,22 @@ async function logJSONData() {
   const jsonData = await response.json();
   console.log(jsonData);
   // fetch로 data.json를 가져와주는 코드
+
+  for(let i=0; i<jsonData.length; i++) {
+    // data.json의 배열의 길이만큼 반복
+    const sections = document.querySelectorAll('section');
+    // html의 section 전부를 선택자로 설정
+    const dataName = document.createElement('li');
+    // li태그를 생성한다.
+    
+    dataName.textContent = `${jsonData[i].title}`;
+    // li태그의 내용은 작성자: json.data의 name, 제목: json.data의 title.
+    
+    sections[1].appendChild(dataName);
+    // 2번째 섹션의 자식요소로 dataName이 들어감.
+    };
 }
+
 logJSONData();
 
 
