dba5dd86edd7594c73a934a83d15fa674d425a5d
JeonSunil <jeonsunil1225@gmail.com>
2025-02-20 11:00:33 +0900

    서버를 localhost:8000 포트에서 서버 실행, 각 코드에 주석 추가

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require('http');
+// http 모듈을 사용하겠다고 선언하는 처리
 const fs = require('fs');
+// fs포듈을 사용하겠다고 선언하는 처리
 
 const server = http.createServer(function(req, res) {
-
+// server라는 변수에 서버를 생성하는 함수를 담는다.
 });
+
+
+const PORT = 8000
+// PORT라는 상수에 8000이라는 값을 선언하고,
+server.listen(PORT, function() {
+  // 서버를 실행한다. PORT번호는 위에서 상수선언한 PORT의 값 8000
+  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+  // 서버가 실행되면, 콘솔에 서버가 실행중이라는 문구가 나타난다.
+})
