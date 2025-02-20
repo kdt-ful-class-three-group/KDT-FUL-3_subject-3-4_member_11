3794c8c804ccbf21aef0a73ad7273215505fd366
JeonSunil <jeonsunil1225@gmail.com>
2025-02-20 17:49:42 +0900

    인터넷에서 json을 불러오는 방법을 검색하고, 주변 사람들에게 물어물어 fetch의 존재를 알게 되었고, fetch로 data.json을 src.js에 불러오게 만들었다.

--- src.js
+++ src.js
@@ -2,17 +2,8 @@ async function logJSONData() {
   const response = await fetch("./data.json");
   const jsonData = await response.json();
   console.log(jsonData);
-
-  for(let i=0; i<jsonData.length; i++) {
-    const sections = document.querySelectorAll('section');
-    const dataName = document.createElement('li');
-    
-    dataName.textContent = `작성자: ${jsonData[i].name} 제목: ${jsonData[i].title}`
-    
-    sections[1].appendChild(dataName);
-    };
+  // fetch로 data.json를 가져와주는 코드
 }
-
 logJSONData();
 
 
