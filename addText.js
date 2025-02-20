3bff5e1bb75bc8fa4a0ab09aa35f1f2c13f36b7e
JeonSunil <jeonsunil1225@gmail.com>
2025-02-20 13:09:18 +0900

    블로그 형식으로 제작할 것이라 원래 있던 json파일을 삭제하고, title input하나를 추가했다.

--- addText.js
+++ addText.js
@@ -2,6 +2,7 @@ function addText() {
   return `<a href = "/">홈</a><a href = "/list">글 목록</a><a href = "/add">글 작성</a>
                   <form action="/data" method="post">
                     <input type="text" name="name" placeholder="name" required>
+                    <input type="text" name="title" placeholder="title" required>
                     <input type="text" name="main" placeholder="main" required>
                     <button type="submit">작성</button>
                   </form>`
