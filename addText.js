eb63ceff6533a68f435bf8e33f8e18acedca48d1
JeonSunil <jeonsunil1225@gmail.com>
2025-02-20 12:06:01 +0900

    데이터 유효성 검사를 위해 input태그에 required 속성 추가

--- addText.js
+++ addText.js
@@ -1,8 +1,8 @@
 function addText() {
   return `<a href = "/">홈</a><a href = "/list">글 목록</a><a href = "/add">글 작성</a>
                   <form action="/data" method="post">
-                    <input type="text" name="name" placeholder="name">
-                    <input type="text" name="main" placeholder="main">
+                    <input type="text" name="name" placeholder="name" required>
+                    <input type="text" name="main" placeholder="main" required>
                     <button type="submit">작성</button>
                   </form>`
 }
