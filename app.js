d2aacfab6c19c0240f7c561ef7440d3ac5454a28
JeonSunil <jeonsunil1225@gmail.com>
2025-02-24 12:29:23 +0900

    수정 페이지에서, /delete로 데이터를 포스트로 보내면, 아래의 post요청에서 updateDataForm의 모듈로, 수정된 데이터를 기존 데이터에 덮어씌운 데이터를 data.json파일에 담고, 성공적으로 수정이 완료 되었다는 문구를 내보내도록 만들었다.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const htmlForm = require('./htmlForm');
 const resForm = require('./resForm');
 const dataForm = require('./dataForm');
 const updateForm = require('./updateForm');
+const updateDataForm = require('./updateDataForm');
 // htmlForm 모듈을 사용하겠다고 선언
 
 
@@ -33,8 +34,8 @@ const server = http.createServer(function(req, res) {
       // i라는 변수에 update의 순서를 가져옴.
       // 글 상세 페이지 요청에 대한 get요청 처리
        res.writeHead(200, {'content-type': `text/html; charset=utf-8`});
-            res.write(updateForm(i));
-            res.end();
+       res.write(updateForm(i));
+       res.end();
     } else
     if(req.url.endsWith('.js')) {
       // 글 상세 페이지 요청에 대한 get요청 처리
@@ -55,6 +56,11 @@ const server = http.createServer(function(req, res) {
     if(req.url === '/data') {
       // 글을 작성하면 액션은 /data, 메서드는 POST로 요청한다.
       dataForm(req, res);
+    } 
+    if(req.url.startsWith('/update')) {
+      const i = req.url.split('update')[1];
+      // 글을 작성하면 액션은 /data, 메서드는 POST로 요청한다.
+      updateDataForm(req, res, i);
     } else {
       res.writeHead(404, {'content-type': 'text/html; charset=utf-8'});
       res.end('404 Not Found');
