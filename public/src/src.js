b9db90ba29eaa60218f00046a417b55e151ad343
JeonSunil <jeonsunil1225@gmail.com>
2025-02-25 15:12:48 +0900

    원래 내용

--- public/src/src.js
+++ public/src/src.js
@@ -29,38 +29,30 @@ async function logJSONData() {
     // li태그를 전부 불러온다.
     
     // ? 새로운 섹션 생성
-    let variable = ['h1', 'dataName', 'dataMain', 'btnArticle', 'updateBtn', 'deleteBtn', 'form', 'inputName', 'inputTitle', 'textarea', 'button', 'delForm', 'delInfo', 'delInput', 'delBtn'];
-    let tagName = ['h1', 'li', 'li', 'article', 'button', 'button', 'form', 'input', 'input', 'textarea', 'button', 'form', 'div', 'input', 'button'];
 
-    function makeElement(variable, tagName) {
-      for(i=0; i<variable.length; i++) {
-      variable[i] = document.createElement(tagName[i]);
-    }}
-
-    makeElement(variable, tagName);
-    // const h1 = document.createElement('h1');
-    // // 새로운 h1태그 생성
-    // const dataName = document.createElement('li');
-    // // 새로운 li태그 생성
-    // const dataMain = document.createElement('li');
-    // // 새로운 li태그 생성
-    // const btnArticle = document.createElement('article');
-    // // 버튼들이 들어갈 article태그 생성
-    // const updateBtn = document.createElement('button');
-    // // 수정 버튼을 a태그로 생성
-    // const deleteBtn = document.createElement('button');
-    // // 삭제 버튼을 a태그로 생성
-    // // * 상세페이지
-    // const form = document.createElement('form');
-    // const inputName = document.createElement('input');
-    // const inputTitle = document.createElement('input');
-    // const textarea = document.createElement('textarea');
-    // const button = document.createElement('button');
-    // // * 수정페이지
-    // const delForm = document.createElement('form');
-    // const delInfo = document.createElement('div');
-    // const delInput = document.createElement('input');
-    // const delBtn = document.createElement('button');
+    const h1 = document.createElement('h1');
+    // 새로운 h1태그 생성
+    const dataName = document.createElement('li');
+    // 새로운 li태그 생성
+    const dataMain = document.createElement('li');
+    // 새로운 li태그 생성
+    const btnArticle = document.createElement('article');
+    // 버튼들이 들어갈 article태그 생성
+    const updateBtn = document.createElement('button');
+    // 수정 버튼을 a태그로 생성
+    const deleteBtn = document.createElement('button');
+    // 삭제 버튼을 a태그로 생성
+    // * 상세페이지
+    const form = document.createElement('form');
+    const inputName = document.createElement('input');
+    const inputTitle = document.createElement('input');
+    const textarea = document.createElement('textarea');
+    const button = document.createElement('button');
+    // * 수정페이지
+    const delForm = document.createElement('form');
+    const delInfo = document.createElement('div');
+    const delInput = document.createElement('input');
+    const delBtn = document.createElement('button');
     // * 삭제페이지
 
     h1.textContent = `${jsonData[i].title}`;
