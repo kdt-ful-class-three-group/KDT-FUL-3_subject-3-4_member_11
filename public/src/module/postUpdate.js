c85acbc976452b88cb5a98db40f4e8850a9b21dc
JeonSunil <jeonsunil1225@gmail.com>
2025-02-25 10:31:10 +0900

    서버에서 지정해놓은 i(데이터 순서, url의 순서)를 가져와야하는데, 방법을 잘 모르겠다.

--- public/src/module/postUpdate.js
+++ public/src/module/postUpdate.js
@@ -1,5 +1,4 @@
 function postUpdate(i) {
-
   async function logJSONData() {
   const response = await fetch("./data.json");
   const jsonData = await response.json();
@@ -15,8 +14,8 @@ function postUpdate(i) {
   input[0].value = jsonData[i].name
   input[1].value = jsonData[i].title
   textarea[0].value = jsonData[i].main
-
   }
+
   logJSONData()
 }
 
