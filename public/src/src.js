769a1f4eb69e17c7eaea1806b80d81a1cc006b59
JeonSunil <jeonsunil1225@gmail.com>
2025-02-26 15:01:58 +0900

    setPosition으로, 같은 부모 요소에 자식요소로 들어가는 요소들을 한 배열로 엮어, 함수로 모두 자식요소로 속하게 만들어 주었다.

--- public/src/src.js
+++ public/src/src.js
@@ -21,8 +21,7 @@ async function logJSONData() {
     // ! 리스트
 
   for(let i=0; i<jsonData.length; i++) {
-    const main = document.querySelector('main');
-    // 메인 태그를 불러온다.
+
     const sections = document.querySelectorAll('section');
     // 섹션 태그 전부를 불러온다.
     const li = document.querySelectorAll('li');
@@ -35,9 +34,23 @@ async function logJSONData() {
     function makeElement(tagName) {
       for(let n=0; n<tagName.length; n++) {
       tagName[n] = document.createElement(tagName[n]);
-    }}
+    }};
 
     makeElement(tagName);
+
+    function setPosition(parent, baby) {
+      for(let i=0; i<baby.length; i++) {
+      parent.appendChild(baby[i]);
+      }
+    };
+
+    function setType(tagName, name, value) {
+      tagName.type = 'text';
+      tagName.name = name;
+      tagName.placeholder = name;
+      tagName.required = 'true';
+      tagName.value = value
+    };
 
     tagName[0].textContent = `${jsonData[i].title}`;
     // h1태그에는 jsondata의 제목을 넣는다.
@@ -49,7 +62,6 @@ async function logJSONData() {
     tagName[3].style.flexDirection = 'row';
     tagName[3].style.justifyContent = 'flex-end';
     // 버튼아티클 스타일 지정
-
     tagName[4].textContent = `수정`;
     // 수정버튼에 수정이라는 글자를 넣어준다.
     tagName[5].textContent = `삭제`;
@@ -58,16 +70,17 @@ async function logJSONData() {
     
 
     li[i].addEventListener('click', function() {
+      let babys = [tagName[0], tagName[1], tagName[2]];
       // 어떤 순서의 li를 클릭하면
       sections[1].style.display = 'none'
       // 2번쨰 섹션이 display none 되고,
-
-      sections[2].appendChild(tagName[0]);
-      // h1태그 는 새로운 섹션에
-      sections[2].appendChild(tagName[1]);
-      // dataName도 새로운 섹션에
-      sections[2].appendChild(tagName[2]);
-      // dataMain도 새로운 섹션에
+      setPosition(sections[2], babys);
+      // sections[2].appendChild(tagName[0]);
+      // // h1태그 는 새로운 섹션에
+      // sections[2].appendChild(tagName[1]);
+      // // dataName도 새로운 섹션에
+      // sections[2].appendChild(tagName[2]);
+      // // dataMain도 새로운 섹션에
       tagName[3].appendChild(tagName[4]);
       // 수정버튼은 버튼아티클에
       tagName[3].appendChild(tagName[5]);
@@ -80,45 +93,24 @@ async function logJSONData() {
 
     tagName[4].addEventListener('click', function() {
       sections[2].style.display = 'none'
-      function setType(tagName, name, value) {
-        tagName.type = 'text';
-        tagName.name = name;
-        tagName.placeholder = name;
-        tagName.required = 'true';
-        tagName.value = value
-      }
 
       tagName[6].action = `/update${i}`;
       tagName[6].method = 'post'
 
       setType(tagName[7], 'name', jsonData[i].name);
-      // tagName[7].type = 'text';
-      // tagName[7].name = 'name';
-      // tagName[7].placeholder = 'name';
-      // tagName[7].required = 'true';
-      // tagName[7].value = jsonData[i].name
-
       setType(tagName[8], 'title', jsonData[i].title);
-      // tagName[8].type = 'text';
-      // tagName[8].name = 'title';
-      // tagName[8].placeholder = 'title';
-      // tagName[8].required = 'true';
-      // tagName[8].value = jsonData[i].title
-      
       setType(tagName[9], 'main', jsonData[i].main);
-      // tagName[9].type = 'text';
-      // tagName[9].name = 'main';
-      // tagName[9].placeholder = 'main';
-      // tagName[9].required = 'true';
-      // tagName[9].value = jsonData[i].main
+
 
       tagName[10].type = 'submit';
       tagName[10].textContent = '작성'
 
-      tagName[6].appendChild(tagName[7]);
-      tagName[6].appendChild(tagName[8]);
-      tagName[6].appendChild(tagName[9]);
-      tagName[6].appendChild(tagName[10]);
+      let babys = [tagName[7], tagName[8], tagName[9], tagName[10]];
+      setPosition(tagName[6], babys)
+      // tagName[6].appendChild(tagName[7]);
+      // tagName[6].appendChild(tagName[8]);
+      // tagName[6].appendChild(tagName[9]);
+      // tagName[6].appendChild(tagName[10]);
 
       sections[3].appendChild(tagName[6])
     });
@@ -131,17 +123,17 @@ async function logJSONData() {
       tagName[11].action = `/delete${i}`;
       tagName[11].method = 'post';
       tagName[12].textContent = '삭제를 원하신다면 아래에 삭제 라고 입력해 주십시오.';
-      tagName[13].type = 'text';
-      tagName[13].name = 'delete';
+      setType(tagName[13], 'delete', '');
       tagName[13].placeholder = '삭제';
       tagName[13].pattern = '삭제';
-      tagName[13].required = 'true';
+
       tagName[14].type = 'submit';
       tagName[14].textContent = '삭제';
-
-      tagName[11].appendChild(tagName[12]);
-      tagName[11].appendChild(tagName[13]);
-      tagName[11].appendChild(tagName[14]);
+      let babys = [tagName[12], tagName[13], tagName[14]];
+      setPosition(tagName[11], babys);
+      // tagName[11].appendChild(tagName[12]);
+      // tagName[11].appendChild(tagName[13]);
+      // tagName[11].appendChild(tagName[14]);
       
       sections[4].appendChild(tagName[11]);
     })
